740c0913681057203100f085e853dfbdf6814b95
Eric <mugoericpg20@gmail.com>
2023-04-27 13:18:53 +0300

    Implemented useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,17 @@
 import React, { useState, useEffect } from "react";
 
 
+function App(){
+    const [dogImage, setDogImage] = useState('');
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(r => r.json())
+        .then(data => setDogImage(data.image))
+        .catch(error => console.error(error))
+    }, [];
+
+
+
+    )
+}
